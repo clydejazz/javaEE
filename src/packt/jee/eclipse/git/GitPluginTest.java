db596886b5e0d1f7f5d70837ba5c5231411ab2d8
Cortright, Clyde <Clyde.Cortright@Nuance.com>
2017-08-01 15:20:02 -0400

    2nd try to commit and push to gitHub

--- src/packt/jee/eclipse/git/GitPluginTest.java
+++ src/packt/jee/eclipse/git/GitPluginTest.java
@@ -6,5 +6,6 @@ public class GitPluginTest {
 		
 		System.out.println("hi from git, from branch bug#1234 !!");
 		System.out.println("trying to commit to gitHub.");
+		System.out.println("trying to commit to gitHub AGAIN.");
 	}
 }
