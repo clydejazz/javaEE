870d162dba743a940e337f8e48fea3f19dd362a3
Cortright, Clyde <Clyde.Cortright@Nuance.com>
2017-08-01 14:17:54 -0400

    from branch bug#1234

--- src/packt/jee/eclipse/git/GitPluginTest.java
+++ src/packt/jee/eclipse/git/GitPluginTest.java
@@ -4,6 +4,6 @@ public class GitPluginTest {
 
 	public static void main(String[] args){
 		
-		System.out.println("hi from git first project");
+		System.out.println("hi from git, from branch bug#1234 !!");
 	}
 }
