618553f1821c1352f4321e486575c6d158aa7fc3
Cortright, Clyde <Clyde.Cortright@Nuance.com>
2017-08-01 15:08:21 -0400

    trying to commit to gitHub

--- src/packt/jee/eclipse/git/GitPluginTest.java
+++ src/packt/jee/eclipse/git/GitPluginTest.java
@@ -5,5 +5,6 @@ public class GitPluginTest {
 	public static void main(String[] args){
 		
 		System.out.println("hi from git, from branch bug#1234 !!");
+		System.out.println("trying to commit to gitHub.");
 	}
 }
